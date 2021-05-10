880fa61431a56422293e48489ff9b1333b4ad5cf
vhoagamo <vhoagamo@gmail.com>
2021-05-10 22:48:27 +0400

    Add project files.

---
+++ GloboTicket.Services.ShoppingBasket/Profiles/BasketLineProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using GloboTicket.Services.ShoppingBasket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GloboTicket.Services.ShoppingBasket.Profiles
+{
+    public class BasketLineProfile : Profile
+    {
+        public BasketLineProfile()
+        {
+            CreateMap<BasketLineForCreation, BasketLine>();
+            CreateMap<BasketLineForUpdate, BasketLine>();
+            CreateMap<BasketLine, BasketLine>().ReverseMap();
+
+        }
+    }
+}
